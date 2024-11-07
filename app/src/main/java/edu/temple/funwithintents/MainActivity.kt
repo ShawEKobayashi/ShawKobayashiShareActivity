72761f0d4921b44309bfe166ecd8a3cff297585b
tuk60682 <kobayashi.shaw.e@gmail.com>
2024-11-07 11:37:02 -0500

    Added a new intent in onClickListener, with action intent, an extra that is editText.Text, and type ="text/plain", then started the activity with the stored intent.

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,10 +1,12 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
+val INTENT_STRING = "intent_string"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            val sendIntent = Intent().apply{
+                action = Intent.ACTION_SEND
+                putExtra(INTENT_STRING, editText.text.toString())
+                type="text/plain"
+            }
+            startActivity(sendIntent)
         }
     }
 }
